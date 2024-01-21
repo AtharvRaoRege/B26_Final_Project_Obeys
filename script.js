671c35d4de88a2f55c284d5f4daaede524cabc63
AtharvRaoRege <atharvrege722@gmail.com>
2024-01-21 17:56:39 +0530

    second commit

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
 });
+
+let loader = document.querySelector('.loadd h3');
+
+let det = 0
+ setInterval(()=>{
+    if(det < 100){
+        console.log(det)
+        det++;
+        loader.innerHTML = det;
+    }
+},40)
